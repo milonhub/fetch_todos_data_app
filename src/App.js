8c072300dfd91a12215ae0268f16bf748dca58f0
milonhub <milonru150@gmail.com>
2023-10-14 10:14:16 -0700

    m

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import Users from './Components/Todos_User';
+// import style from './Components/todos.module.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="APP">
       <Users />
     </div>
   );
--- src/Components/Todos_User.js
+++ src/Components/Todos_User.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import {useSelector, useDispatch } from 'react-redux'
-import getAllTodos from '../Services/action/todosAction'
+import getAllTodos from '../Services/action/todosAction';
+import style from './todos.module.css';
 
 const Users = () => {
 
@@ -16,14 +17,21 @@ const Users = () => {
        
   return (
     <div> 
+          <h1>Using React-Redux</h1>
+          <h1>User Data Management App</h1>
+
       {isLoading && <h3> Data is loading ....</h3>}
       {error && <h3>{error}</h3>}
+      <section className={style.section}>
       {todos.map((data)=> {
-        return <section key={data.id}>
-          <p>{data.title}</p>
+        return <article key={data.id} className={style.article}>
           <p>{data.id}</p>
-        </section>
+          <p>{data.title}</p> 
+        </article>
       })} 
+      </section>
+     
+      
     </div>
   )
 }
